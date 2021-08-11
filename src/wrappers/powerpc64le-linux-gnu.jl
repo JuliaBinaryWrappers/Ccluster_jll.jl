2ebd8a5a8bd0fee292e9cf6a46b0d653c63bdf6f
jlbuild <juliabuildbot@gmail.com>
2021-08-11 14:23:19 +0000

    Ccluster_jll build 1.1.7+0

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -6,7 +6,7 @@ using FLINT_jll
 using GMP_jll
 using MPFR_jll
 JLLWrappers.@generate_wrapper_header("Ccluster")
-JLLWrappers.@declare_library_product(libccluster, "libccluster.so.1.1.6")
+JLLWrappers.@declare_library_product(libccluster, "libccluster.so.1.1.7")
 function __init__()
     JLLWrappers.@generate_init_header(Arb_jll, FLINT_jll, GMP_jll, MPFR_jll)
     JLLWrappers.@init_library_product(
